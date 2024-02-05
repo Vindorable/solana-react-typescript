1e862cecb225aae1b1249f840c47cc88a99bc26b
Vindorable <j.raj93.designer@gmail.com>
2024-02-05 15:53:28 +0800

    Create mint token page.

---
+++ solana-webapp/src/MintToken.tsx
@@ -0,0 +1,21 @@
+
+
+
+function MintToken() {
+  return (
+    <>
+      <div>
+        Mint Token Section
+        <div>
+          <button>Create Token</button>
+          <button>Mint Token</button>
+          <button>Check Balance</button>
+          <button>Send Token</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+export default MintToken
