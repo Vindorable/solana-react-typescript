9f82a1a3831fcf54cd09262671638135089dc043
Vindorable <j.raj93.designer@gmail.com>
2024-02-06 13:57:43 +0800

    Added functions to create and mint NFT.

--- solana-webapp/src/MintNFT.tsx
+++ solana-webapp/src/MintNFT.tsx
@@ -21,7 +21,7 @@ import {
 //window.Buffer = window.Buffer || require("buffer").Buffer;
 
 
-function MintToken() {
+function MintNFT() {
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
   // Since we are doing this on the devnet, we don't have to connect to a wallet.
@@ -32,68 +32,41 @@ function MintToken() {
   let mint: PublicKey;
   let fromTokenAccount: Account;
 
-  const toWallet = new PublicKey("2VeLziagu5mqx4JpVD9UAoTcir5xLYJyd5U5QgwCnVH1");
-
-  async function createToken() {
+  async function createNFT() {
     // We need some sols for our operations.
     const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
     await connection.confirmTransaction(fromAirdropSignature);
 
-    // Create new token mint.
+    // Create new NFT mint.
     mint = await createMint(
       connection,
       fromWallet,
       fromWallet.publicKey,
       null,
-      9 // (decimal) smallest number value of out token.
+      0 // Only allow whole tokens.
     );
-    console.log(`Create token: ${mint.toBase58()}`);
+    console.log(`Create NFT: ${mint.toBase58()}`);
 
-    // Get the account that minted the token, else create the associated account.
+    // Get the account that minted the NFT, else create the associated account.
     fromTokenAccount = await getOrCreateAssociatedTokenAccount(
       connection,
       fromWallet,
       mint,
       fromWallet.publicKey
     );
-    console.log(`Create Token Account: ${fromTokenAccount.address.toBase58()}`);
+    console.log(`Create NFT Account: ${fromTokenAccount.address.toBase58()}`);
   }
 
-  async function mintToken() {
+  async function mintNFT() {
     const signature = await mintTo(
       connection,
       fromWallet,
       mint,
       fromTokenAccount.address,
       fromWallet.publicKey,
-      10000000000 // 10 billion = 10 tokens because of the 9 decimal.
+      1
     );
     console.log(`Mint Signature: ${signature}`);
-  }
-
-  async function checkBalance() {
-    // Check the supply of tokens we have minted into existance.
-    const mintInfo = await getMint(connection, mint);
-    console.log(mintInfo.supply);
-
-    // Get the amount of tokens left in the account.
-    const tokenAccountInfo = await getAccount(connection, fromTokenAccount.address);
-    console.log(tokenAccountInfo.amount);
-  }
-
-  async function sendToken() {
-    const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet);
-    console.log(`toTokenAccount: ${toTokenAccount.address}`);
-
-    const signature = await transfer(
-      connection,
-      fromWallet,
-      fromTokenAccount.address,
-      toTokenAccount.address,
-      fromWallet.publicKey,
-      1000000000 // 1 billion.
-    );
-    console.log(`Finished transfer with: ${signature}`);
   }
 
   return (
@@ -101,10 +74,9 @@ function MintToken() {
       <div>
         Mint Token Section
         <div>
-          <button onClick={createToken}>Create Token</button>
-          <button onClick={mintToken}>Mint Token</button>
-          <button onClick={checkBalance}>Check Balance</button>
-          <button onClick={sendToken}>Send Token</button>
+          <button onClick={createNFT}>Create NFT</button>
+          <button onClick={mintNFT}>Mint NFT</button>
+          <button onClick={lockNFT}>Lock NFT</button>
         </div>
       </div>
     </>
@@ -112,4 +84,4 @@ function MintToken() {
 }
 
 
-export default MintToken
+export default MintNFT
